98993cb6f5a89a37c62da544efcfc52b0c975611
MON@H <monah.rasta@gmail.com>
2020-10-02 19:49:27 +0300

    Added check name for spam regex list

--- AntiSpamNames.cs
+++ AntiSpamNames.cs
@@ -1,52 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
-using Newtonsoft.Json;
-using Oxide.Core.Libraries.Covalence; 
+using System;
 
 namespace Oxide.Plugins
 {
-    [Info("Anti Spam Names", "Ultra", "1.1.2")]
+    [Info("Anti Spam Names", "Ultra", "1.2.0")]
     [Description("Replaces domain extensions or specific spam words in player's name. Also renames fake admins.")]
 
     class AntiSpamNames : CovalencePlugin
     {
-        #region Hooks
+        #region Initialization
 
-        void OnUserConnected(IPlayer player)
+        private void OnServerInitialized()
         {
-            string newName = player.Name;
-
-            if (configData.CheckSpamNames)
+            foreach (var player in players.Connected)
             {
-                foreach (string spamKeyword in configData.SpamKeywordBlacklist)
-                {
-                    if (player.Name.ToLower().Contains(spamKeyword.ToLower()))
-                    {
-                        Regex regex = new Regex(spamKeyword, RegexOptions.IgnoreCase);
-                        newName = regex.Replace(newName, configData.ReplaceForSpam);
-                    }
-                }
-            }
-
-            if (configData.CheckAdminNames && !player.IsAdmin)
-            {
-                foreach (string adminName in configData.AdminNameBlacklist)
-                {
-                    if (player.Name.ToLower().Contains(adminName.ToLower()))
-                    {
-                        Regex regex = new Regex(adminName, RegexOptions.IgnoreCase);
-                        newName = regex.Replace(newName, configData.ReplaceForAdmin);
-                    }
-                }
-            }
-
-            if (player.Name.ToLower() != newName.ToLower())
-            {
-                player.Rename(newName);
+                HandleName(player);
             }
         }
 
-        #endregion
+        #endregion Initialization
+
 
         #region Config
 
@@ -54,6 +31,12 @@ namespace Oxide.Plugins
 
         private class ConfigData
         {
+            [JsonProperty(PropertyName = "Allow check name for spam regex list")]
+            public bool UseRegexList;
+
+            [JsonProperty(PropertyName = "Print to log all name changes")]
+            public bool PrintEnabled;
+
             [JsonProperty(PropertyName = "Check admin names")]
             public bool CheckAdminNames;
 
@@ -69,8 +52,14 @@ namespace Oxide.Plugins
             [JsonProperty(PropertyName = "Replace for spam")]
             public string ReplaceForSpam;
 
+            [JsonProperty(PropertyName = "Replace if empty (whole name filtered)")]
+            public string ReplaceForEmpty;
+
             [JsonProperty(PropertyName = "Spam keyword blacklist")]
             public List<string> SpamKeywordBlacklist;
+
+            [JsonProperty(PropertyName = "Spam regex list")]
+            public List<string> SpamRegexList;
         }
 
         protected override void LoadConfig()
@@ -96,13 +85,24 @@ namespace Oxide.Plugins
         {
             configData = new ConfigData()
             {
-                CheckAdminNames = true,
-                CheckSpamNames = true,
+                CheckAdminNames = false,
+                CheckSpamNames = false,
+                UseRegexList = false,
+                PrintEnabled = true,
                 AdminNameBlacklist = new List<string>() { "Administrator", "Admin" },
                 ReplaceForAdmin = "Player",
                 ReplaceForSpam = "Spam",
-                SpamKeywordBlacklist = new List<string>() { ".money", ".ru", ".com", ".pl", ".gg", ".de", ".net", "www.", ".org", ".info", ".cz", ".sk", ".uk", ".cn", ".nl", ".store", ".shop" }
-
+                ReplaceForEmpty = "Good name",
+                SpamKeywordBlacklist = new List<string>() { ".money", ".ru", ".com", ".pl", ".gg", ".de", ".net", "www.", ".org", ".info", ".cz", ".sk", ".uk", ".cn", ".nl", ".store", ".shop" },
+                SpamRegexList = new List<string>()
+                {
+                    "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",
+                    "(:\\d{3,5})",
+                    "([Ааa4][Ддd][Ммm][Ииi1][Ннn])",
+                    "(https|http|ftp|):\\/\\/",
+                    "((\\p{L}+|[0-9]+)+\\.)+(com|org|net|int|edu|gov|mil|ch|cn|co|de|eu|fr|in|nz|ru|tk|tr|uk|us)",
+                    "((\\p{L}+|[0-9]+)+\\.)+(ua|pro|io|dev|me|ml|tk|ml|ga|cf|gq|tf)"
+                }
             };
         }
 
@@ -110,6 +110,90 @@ namespace Oxide.Plugins
         {
             Config.WriteObject(configData, true);
             base.SaveConfig();
+        }
+
+        #endregion
+
+        #region Hooks
+
+        private void OnUserConnected(IPlayer player)
+        {
+            HandleName(player);
+        }
+
+        private void OnUserNameUpdated(string id, string oldName, string newName)
+        {
+            if (oldName == newName) return;
+
+            IPlayer player = players.FindPlayerById(id);
+            HandleName(player);
+        }
+
+        private string GetClearName(IPlayer player)
+        {
+            string newName = player.Name;
+            if (configData.UseRegexList)
+            {
+                newName = GetClearText(newName, configData.SpamRegexList, configData.ReplaceForSpam);    
+            }
+
+            if (configData.CheckSpamNames)
+            {
+                foreach (string spamKeyword in configData.SpamKeywordBlacklist)
+                {
+                    if (player.Name.ToLower().Contains(spamKeyword.ToLower()))
+                    {
+                        Regex regex = new Regex(spamKeyword, RegexOptions.IgnoreCase);
+                        newName = regex.Replace(newName, configData.ReplaceForSpam);
+                    }
+                }
+            }
+
+            if (configData.CheckAdminNames && !player.IsAdmin)
+            {
+                foreach (string adminName in configData.AdminNameBlacklist)
+                {
+                    if (player.Name.ToLower().Contains(adminName.ToLower()))
+                    {
+                        Regex regex = new Regex(adminName, RegexOptions.IgnoreCase);
+                        newName = regex.Replace(newName, configData.ReplaceForAdmin);
+                    }
+                }
+            }
+
+            return newName;
+        }
+
+        private string GetClearText(string text, List<string> regexList, string replacement = null)
+        {
+            if (string.IsNullOrEmpty(text)) return string.Empty;
+
+            foreach (var SpamRegex in regexList)
+            {
+                Regex rgx = new Regex(SpamRegex, RegexOptions.IgnoreCase);
+                text = rgx.Replace(text, string.IsNullOrEmpty(replacement) ? string.Empty : replacement);
+            }
+
+            return text;
+        }
+
+        private void HandleName(IPlayer player)
+        {
+            if (player == null || !player.IsConnected) return;
+            string newName = GetClearName(player);
+
+            if (!player.Name.Equals(newName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (newName.Trim().Count() == 0)
+                {
+                    newName = string.IsNullOrEmpty(configData.ReplaceForEmpty) ? "Good Name" : configData.ReplaceForEmpty;
+                }
+                if (configData.PrintEnabled)
+                {
+                    Puts("Renaming a player (" + player.Id + "): oldname=" + player.Name + ", newname=" + newName);
+                }
+                player.Rename(newName);
+            }
         }
 
         #endregion
--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-# Configuration
+##  Configuration
 Just specify 'Spam keyword blacklist' with words (or domain extensions) you want to avoid. These words will be replaced with 'Replace for spam' expression.
-
 You can also control fake admin players. Use 'Admin name blacklist' to specify words to avoid for non-admin players. It will ignore real administrators of course.
 
-```
+Now supports regex! Default regex matches `IP`, `port`, `domain` with `subdomains` and `admin` word.
+Regex list check is case insensitive by the design (at least for english). Replaces founded with `Replace for spam`.
+All checks are disabled by default, so you can install plugin safely and then configuration default config to your needs.
+You probably want to try enabling only regex list first, as it may be all you need.
+Afterwards you can always enable additional checks if needed.
+
+```json
 {
   "Admin name blacklist": [
     "Administrator",
     "Admin"
   ],
-  "Replace for admin": "justplayer",
-  "Replace for spam": "spam",
+  "Allow check name for spam regex list": false,
+  "Check admin names": false,
+  "Check spam names": false,
+  "Print to log all name changes": true,
+  "Replace for admin": "Player",
+  "Replace for spam": "Spam",
+  "Replace if empty (whole name filtered)": "Good name",
   "Spam keyword blacklist": [
     ".money",
     ".ru",
@@ -18,11 +28,41 @@ You can also control fake admin players. Use 'Admin name blacklist' to specify w
     ".pl",
     ".gg",
     ".de",
-    ".net"
+    ".net",
+    "www.",
+    ".org",
+    ".info",
+    ".cz",
+    ".sk",
+    ".uk",
+    ".cn",
+    ".nl",
+    ".store",
+    ".shop"
   ],
-  "Check admin names": true,
-  "Check spam names": true
+  "Spam regex list": [
+    "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",
+    "(:\\d{3,5})",
+    "([类a4][匿d][天m][辱i1][晚n])",
+    "(https|http|ftp|):\\/\\/",
+    "((\\p{L}+|[0-9]+)+\\.)+(com|org|net|int|edu|gov|mil|ch|cn|co|de|eu|fr|in|nz|ru|tk|tr|uk|us)",
+    "((\\p{L}+|[0-9]+)+\\.)+(ua|pro|io|dev|me|ml|tk|ml|ga|cf|gq|tf)"
+  ]
 }
+```
+
+## API
+
+```cs
+string GetClearText(string text, List<string> regexList, string replacement)
+//will return string with all regexList matches replaced with passed replacement
+/*
+myDirtyText = "Administrator"
+replacement = "Spam"
+List<string> regexList = new List<string>() {"([类a4][匿d][天m][辱i1][晚n])"}
+var myClearedText = GetClearText(myDirtyText, regexList, replacement)
+// myClearedText will be = "Spamistrator"
+*/
 ```
 
 I would appreciate any feedback or ideas to improve this plugin.
